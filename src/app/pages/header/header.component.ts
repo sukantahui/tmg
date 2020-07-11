cd17f8ad824fc7b145c5fc3cb0e3eabdaef8c5d6
sukanta hui <huisukanta77@gmail.com>
2020-07-11 10:38:02 +0530

    host listener added but not working scroll

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,22 +1,27 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Subscription} from 'rxjs';
 import {User} from '../../models/user.model';
 import {Router} from '@angular/router';
 import {GeneralService} from '../../services/general.service';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit {
+  header_variable = false;
+  // tslint:disable-next-line:align
+  @HostListener('document:scroll', [])
+  onWindowScroll() {
+    console.log('You just scolled');
+  }
+  // tslint:disable-next-line:align
   constructor(public generalService: GeneralService) { }
 
   ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
   }
 
   myStyle(){
